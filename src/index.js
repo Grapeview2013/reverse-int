0744618f6a5f9315e649baf53ef945e571d0f8ac
Grapeview2013 <aigulmus@gmail.com>
2023-02-17 22:09:11 +0600

    problem solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function reverse (n) {
+    // Convert the number to a string
+  let numberString = n.toString();
   
+  // Split the string into an array of individual digits
+  let regularDigits = numberString.split("");
+  
+  // Reverse the array of digits
+  let reversedDigits = regularDigits.reverse();
+  
+  // Join the array of reversed digits back into a string
+  let reversedString = reversedDigits.join("");
+
+  
+  // Convert the reversed string back to a number and return it
+  return parseInt(reversedString);
 }
